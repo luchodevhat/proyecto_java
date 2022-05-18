6ca4268888b8e07a9d26c5411b7321de9b73cba3
Luis Alejandro <alfaroalejandro597@gmail.com>
2022-05-17 20:59:41 -0600

    Fix: Error al cargar totalidad de archivos iniciales

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -11,7 +11,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola Mundo");
     }
     
 }
