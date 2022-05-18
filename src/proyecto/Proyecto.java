06304fd7096b0b73c5f190904afdd41c480c9313
Luis Alejandro <alfaroalejandro597@gmail.com>
2022-05-17 20:51:48 -0600

    Primer commit

---
+++ src/proyecto/Proyecto.java
@@ -0,0 +1,17 @@
+
+package proyecto;
+
+/**
+ *
+ * @author Aleja
+ */
+public class Proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
