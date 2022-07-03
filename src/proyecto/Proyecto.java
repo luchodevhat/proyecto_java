ee5dab3d7772f39c4532c87437e2ad8d246e1ae7
Luis Alejandro <alfaroalejandro597@gmail.com>
2022-07-03 12:53:56 -0600

    prueba

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -1,17 +1,11 @@
 
 package proyecto;
 
-/**
- *
- * @author Aleja
- */
 public class Proyecto {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+       
     }
     
 }
