0c6b9dd96ce5036e68cf17938ec232ea1b173083
amyblackberry <amandaquesadam05@gmail.com>
2022-08-01 15:39:08 -0600

    Cambios en el menu principal: Mensaje de bienvenida repetido eliminado Pedir el nombre de jugador CPU al usuario

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -6,6 +6,6 @@ public class Proyecto {
     public static void main(String[] args) {
         MenuPrincipal menuDeJuego = new MenuPrincipal();
         menuDeJuego.datosInicialesJugadores();
-
+        
     }
 }
